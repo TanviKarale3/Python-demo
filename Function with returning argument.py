34aafa16107f390c3d7a47ded96b43cbfef90184
Tanvi karale <119518074+TanviKarale3@users.noreply.github.com>
2023-03-22 04:11:43 -0700

    Add files via upload

---
+++ Function with returning argument.py
@@ -0,0 +1,9 @@
+#Calculate a sum of all numbers from 1 to n which will pass as a argument
+
+def sum(a):
+    s=0:
+    for i in range(1,a+1):
+        s=s+1
+        return s
+a=int(Input("Enter a nos=>"))
+print("Sum of all nos,"s)
