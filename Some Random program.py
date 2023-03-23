f18080c9aee6e80478efe917ed9439fefbf141ec
Tanvi karale <119518074+TanviKarale3@users.noreply.github.com>
2023-03-22 20:47:16 -0700

    Add files via upload

---
+++ Some Random program.py
@@ -0,0 +1,78 @@
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+
+
+# Python program to find the largest number among the three input numbers
+
+# change the values of num1, num2 and num3
+# for a different result
+num1 = 10
+num2 = 14
+num3 = 12
+
+# uncomment following lines to take three numbers from user
+#num1 = float(input("Enter first number: "))
+#num2 = float(input("Enter second number: "))
+#num3 = float(input("Enter third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
+
+
+
+#Write a Python Program to check no is positive ,negative or zero
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
+
+
+
+#Write a python program that takes the marks of 5 subject and display there names
+print("Enter Marks Obtained in 5 Subjects: ")
+markOne = int(input())
+markTwo = int(input())
+markThree = int(input())
+markFour = int(input())
+markFive = int(input())
+
+tot = markOne+markTwo+markThree+markFour+markFive
+avg = tot/5
+
+if avg>=91 and avg<=100:
+    print("Your Grade is A1")
+elif avg>=81 and avg<91:
+    print("Your Grade is A2")
+elif avg>=71 and avg<81:
+    print("Your Grade is B1")
+elif avg>=61 and avg<71:
+    print("Your Grade is B2")
+elif avg>=51 and avg<61:
+    print("Your Grade is C1")
+elif avg>=41 and avg<51:
+    print("Your Grade is C2")
+elif avg>=33 and avg<41:
+    print("Your Grade is D")
+elif avg>=21 and avg<33:
+    print("Your Grade is E1")
+elif avg>=0 and avg<21:
+    print("Your Grade is E2")
+else:
+    print("Invalid Input!")
